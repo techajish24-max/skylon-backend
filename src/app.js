59e5ee31c894d96f49e9193135e08b53a551ed6b
Ajish Nair <techajish24@gmail.com>
2026-05-11 13:52:48 +0530

    added pool in import

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import pool from "../src/db/db.js";
 
 import authRoutes from "./routes/authRoutes.js";
 import flatRoutes from "./routes/flatRoutes.js";
